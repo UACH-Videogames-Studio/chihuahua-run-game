1586df7722a2f9c3f5fc53ab61f8eb7eb962c566
FranBulk <149741191+FranBulk@users.noreply.github.com>
2025-03-20 13:01:52 -0600

    Just a few changes in the menu manager script

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -3,13 +3,18 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    [SerializeField] private string gameNameScene;
     public void StartGame()
     {
-        SceneManager.LoadScene("Game");
+        SceneManager.LoadScene(gameNameScene);
     }
-
     public void QuitGame()
     {
-        Application.Quit();
+        // The #if is a special function in C# that activates itself before the build, so if we are in the editor, it quit the game mode, else, it quit the game
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.ExitPlaymode();
+        #else
+           Application.Quit();
+        #endif
     }
 }
