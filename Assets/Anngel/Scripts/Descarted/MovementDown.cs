1051ac6447a16472330226370cb6e6c6ccfc26aa
Ángel Ortiz <anngelo.omz@gmail.com>
2025-03-27 16:15:08 -0600

    Add chacarito run animation in gamplay

---
+++ Assets/Anngel/Scripts/Descarted/MovementDown.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class MovementDown : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public float limitVelocity = 15.0f;
+    public float targetSpeed = 3.0f;
+    public float acceleration = 1.0f;
+
+    void Update()
+    {
+        if (ObstaclesGenerator.isSlowedDown)
+        {
+            targetSpeed = 0.5f;
+        }
+        else
+        {
+            targetSpeed = 3.0f;
+        }
+
+        speed = Mathf.Lerp(speed, targetSpeed, acceleration * Time.deltaTime);
+        speed = Mathf.Min(speed, limitVelocity);
+
+        transform.position += Vector3.down * speed * Time.deltaTime;
+    }
+
+    void OnDestroy()
+    {
+        // Delete this object from allMovementScripts list
+        ObstaclesGenerator.allMovementScripts.Remove(this);
+    }
+}
