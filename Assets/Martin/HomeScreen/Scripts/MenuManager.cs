495a6c1cfde929f5a72bfb10156ba17f9907fc32
Martín Chacón <chacon0754@outlook.com>
2025-03-26 21:57:26 -0600

    Add Next Scene Transition Animation

---
+++ Assets/Martin/HomeScreen/Scripts/MenuManager.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    [Header("Main menu")]
+    //[SerializeField] private string gameNameScene;
+    //[SerializeField] private GameObject optionsPanel;
+    [SerializeField] private CanvasGroup mainMenuCanvasGroup;
+    [SerializeField] private SceneTransitionManager sceneTransitionManager;
+
+    [Header("Options menu")]
+    [SerializeField] private CanvasGroup optionsCanvasGroup;
+
+    public void StartGame()
+    {
+        sceneTransitionManager.LoadNextScene();
+    }
+    public void QuitGame()
+    {
+        // The #if is a special function in C# that activates itself before the build, so if we are in the editor, it quit the game mode, else, it quit the game
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.ExitPlaymode();
+        #else
+           Application.Quit();
+        #endif
+    }
+
+    public void OptionsButton()
+    {
+        optionsCanvasGroup.alpha = 1f;
+        optionsCanvasGroup.interactable = true;
+        optionsCanvasGroup.blocksRaycasts = true;
+        mainMenuCanvasGroup.interactable = false;
+        mainMenuCanvasGroup.blocksRaycasts = false;
+    }
+
+    public void QuitOptionsPanel()
+    {
+        optionsCanvasGroup.alpha = 0f;
+        optionsCanvasGroup.interactable = false;
+        optionsCanvasGroup.blocksRaycasts = false;
+        mainMenuCanvasGroup.interactable = true;
+        mainMenuCanvasGroup.blocksRaycasts = true;
+    }
+
+}
