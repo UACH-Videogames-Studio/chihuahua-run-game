66b2630d61000f8be6d3a734a3e53e412a446dca
Martín Chacón <chacon0754@outlook.com>
2025-04-05 13:18:29 -0600

    Implement dialogue system to game over screen

--- Assets/Game/DialogueSystem/Scripts/DialogueTurn.cs
+++ Assets/Game/DialogueSystem/Scripts/DialogueTurn.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 [System.Serializable]
 public class DialogueTurn
 {
-    [SerializeField] public DialogueCharacterSO Character;
+    //[SerializeField] public DialogueCharacterSO Character;
     [SerializeField, TextArea(2, 4)] private string dialogueLine = string.Empty;
 
     public string DialogueLine => dialogueLine;
